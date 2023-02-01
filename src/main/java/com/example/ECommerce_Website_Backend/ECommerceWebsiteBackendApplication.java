932c3b6e1160ef2258fa3e094c5a7343f04618f4
chunduri bhavana <frootichunduri@gmail.com>
2023-02-01 21:58:46 +0530

    started

---
+++ src/main/java/com/example/ECommerce_Website_Backend/ECommerceWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ECommerce_Website_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceWebsiteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ECommerce_Website_Backend/ECommerceWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ECommerce_Website_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceWebsiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
